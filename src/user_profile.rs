3082c0d67831056f9146bf9fd1d99156deeb18dd
Leonid Amirov <leonid.amirov@gmail.com>
2024-11-12 00:45:14 +0300

    feat(structs): auth-related structs, minimal fields

--- src/user_profile.rs
+++ src/user_profile.rs
@@ -1,7 +1,26 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug)]
 pub struct UserProfile {
     pub id: i64,
     pub email: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct AuthCredentials {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct AuthResponseUserInfo {
+    pub id: i64,
+    pub username: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct AuthResponse {
+    user: AuthResponseUserInfo,
+    token: String,
+    centrifugo_token: String,
 }
